98929d3b2b454f7a3d6e32fc7ba498ccb0e756dd
Tomas Zubik <zubik.tomas@gmail.com>
2021-10-06 21:20:46 -0300

    tp7

--- practica_7/p7ej3.c
+++ practica_7/p7ej3.c
@@ -2,10 +2,36 @@
 // La ordenación también debe realizarla un procedimiento
 #include <stdio.h>
 //#include <string.h>
+int filas = 4;
+int columnas = 50;
 
-int matriz[14][50];
+int ordenar(int x[][], int elec){
+    int aux;
+    for(int i = 0; i< filas; i++){
+        if(x[i][elec] > x[i+1][elec]){
+            for(int c = 0; c< columnas; c++){
+                aux = x[i][c];
+                x[i][c] = x[i+1][c];
+                x[i+1][c] = aux;
+        }
+    }
+}
+
 int main()
 {
-    
+    int ww = 2;
+    int matriz[4][50];
+    for(int fil = 0; fil< filas; fil++){
+        for(int col = 0; col< columnas; col++){
+            matriz[fil][col] = fil;
+        }
+    }
+    for(int fil = 0; fil< filas; fil++){
+        for(int col = 0; col< columnas; col++){
+            printf("%d", matriz[fil][col]);
+        }
+    }
+
+    ordenar(matriz, ww);
     return 0;
 }
